e90bd87ca1b5cf6646a168d1a5aeb45613a2a601
Atikur Rahaman <atikurrahaman0305@gmail.com>
2025-01-03 16:03:49 +0600

    setup the project

---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,25 @@
+import { Response } from 'express';
+
+export type TMeta = {
+  page: number;
+  limit: number;
+  total: number;
+  totalPage: number;
+};
+
+type TResponse<T> = {
+  success: boolean;
+  message?: string;
+  statusCode: number;
+  meta?: TMeta;
+  data: T;
+};
+
+export const sendResponse = <T>(res: Response, data: TResponse<T>) => {
+  res.status(data?.statusCode).json({
+    success: data.success,
+    message: data.message,
+    meta: data.meta,
+    data: data.data,
+  });
+};
